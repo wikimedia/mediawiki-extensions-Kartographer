7384d5bb3e552e2768fafa91996b003dd7b04669
James D. Forrester <jforrester@wikimedia.org>
2016-01-05 10:39:37 -0800

    build: Re-enable the 'requireSpacesInsideParentheses' rule for jscs
    Change-Id: Ic3f6f22c47f98116e516749a7d952fd78513175e

--- modules/kartographer.js
+++ modules/kartographer.js
@@ -2,7 +2,7 @@
 
 	// Load this script after lib/mapbox-lib.js
 
-	var mapServer = mw.config.get( 'wgKartographerMapServer'),
+	var mapServer = mw.config.get( 'wgKartographerMapServer' ),
 		forceHttps = mapServer[ 4 ] === 's',
 		config = L.mapbox.config;
 
@@ -15,13 +15,13 @@
 		var i, scale,
 			brackets = mw.config.get( 'wgKartographerSrcsetScales' ),
 			baseRatio = window.devicePixelRatio || 1;
-		if (!brackets) {
+		if ( !brackets ) {
 			return 1;
 		}
-		brackets.unshift(1);
-		for (i = 0; i < brackets.length; i++) {
+		brackets.unshift( 1 );
+		for ( i = 0; i < brackets.length; i++ ) {
 			scale = brackets[ i ];
-			if (scale >= baseRatio || (baseRatio - scale) < 0.1) {
+			if ( scale >= baseRatio || ( baseRatio - scale ) < 0.1 ) {
 				return scale;
 			}
 		}
@@ -33,40 +33,40 @@
 		var scale, urlFmt, mapData, geoJson, dataLayer;
 
 		scale = bracketDevicePixelRatio();
-		scale = (scale === 1) ? '' : ('@' + scale + 'x');
+		scale = ( scale === 1 ) ? '' : ( '@' + scale + 'x' );
 		urlFmt = '/{z}/{x}/{y}' + scale + '.png';
 		mapData = mw.config.get( 'wgKartographerLiveData' ) || {};
 
-		$content.find('.mw-kartographer-live').each( function () {
-			var $this = $(this),
-				style = $this.data('style'),
-				zoom = $this.data('zoom'),
-				lat = $this.data('lat'),
-				lon = $this.data('lon'),
-				overlays = $this.data('overlays'),
-				map = L.map(this).setView([ lat, lon ], zoom);
-			map.attributionControl.setPrefix('');
-			L.tileLayer(mapServer + '/' + style + urlFmt, {
+		$content.find( '.mw-kartographer-live' ).each( function () {
+			var $this = $( this ),
+				style = $this.data( 'style' ),
+				zoom = $this.data( 'zoom' ),
+				lat = $this.data( 'lat' ),
+				lon = $this.data( 'lon' ),
+				overlays = $this.data( 'overlays' ),
+				map = L.map( this ).setView( [ lat, lon ], zoom );
+			map.attributionControl.setPrefix( '' );
+			L.tileLayer( mapServer + '/' + style + urlFmt, {
 				maxZoom: 18,
 				attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-			}).addTo(map);
+			} ).addTo( map );
 
-			if (overlays) {
+			if ( overlays ) {
 				geoJson = [];
-				$.each(overlays, function (_, group) {
-					if (group === '*') {
-						$.each(mapData, function (k, d) {
-							if (k[ 0 ] !== '_') {
-								geoJson = geoJson.concat(d);
+				$.each( overlays, function ( _, group ) {
+					if ( group === '*' ) {
+						$.each( mapData, function ( k, d ) {
+							if ( k[ 0 ] !== '_' ) {
+								geoJson = geoJson.concat( d );
 							}
-						});
-					} else if (mapData.hasOwnProperty(group)) {
-						geoJson = geoJson.concat(mapData[ group ]);
+						} );
+					} else if ( mapData.hasOwnProperty( group ) ) {
+						geoJson = geoJson.concat( mapData[ group ] );
 					}
-				});
-				dataLayer = L.mapbox.featureLayer().addTo(map);
-				dataLayer.setGeoJSON(geoJson);
+				} );
+				dataLayer = L.mapbox.featureLayer().addTo( map );
+				dataLayer.setGeoJSON( geoJson );
 			}
-		});
+		} );
 	} );
 }( jQuery, mediaWiki ) );
