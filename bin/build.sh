011b5bf39187b71a6d9394e9a8005562d85c3035
Julien Girault <jgirault@wikimedia.org>
2016-06-30 01:06:43 -0700

    Add a resource to load the PruneCluster plugin
    Bug: T136453
    Change-Id: I80d72a91522519ff2759bbfb4f8c062571c4a656

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+node_modules/.bin/browserify lib/mapbox/mapbox-main.js -o lib/mapbox/mapbox-lib.js
+cp -rv node_modules/mapbox.js/theme/* lib/mapbox/
+rm lib/mapbox/images/render.sh
+
+cp -v node_modules/leaflet-sleep/Leaflet.Sleep.js lib/leaflet.sleep.js
+
+cp -v node_modules/prunecluster/dist/PruneCluster.js \
+    node_modules/prunecluster/dist/LeafletStyleSheet.css \
+    lib/prunecluster/
---
+++ modules/prunecluster/prunecluster.js
@@ -0,0 +1,11 @@
+/* globals module, PruneCluster, PruneClusterForLeaflet */
+/**
+ * Exports the PruneCluster library.
+ *
+ * @alias PruneCluster
+ * @class Kartographer.PruneCluster
+ */
+module.exports = {
+	PruneCluster: PruneCluster,
+	PruneClusterForLeaflet: PruneClusterForLeaflet
+};
