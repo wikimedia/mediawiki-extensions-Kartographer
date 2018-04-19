9929770255c2902819e93aebcdc86479cb86c70d
Moriel Schottlender <moriel@gmail.com>
2018-04-19 15:08:57 -0700

    Split the external libraries to lib/external
    The files in lib/external/* are brought in from external sources
    and should not be directly edited.
    
    The files in lib/* are copied over and managed in this repo.
    
    Change-Id: I50c66e1d71c426187ecf9955c25b0b893413fa61

--- bin/build.sh
+++ bin/build.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-node_modules/.bin/browserify lib/mapbox/mapbox-main.js -o lib/mapbox/mapbox-lib.js
-cp -rv node_modules/mapbox.js/theme/* lib/mapbox/
-rm lib/mapbox/images/render.sh
+node_modules/.bin/browserify lib/external/mapbox/mapbox-main.js -o lib/external/mapbox/mapbox-lib.js
+cp -rv node_modules/mapbox.js/theme/* lib/external/mapbox/
+rm lib/external/mapbox/images/render.sh
 
-cp -v node_modules/leaflet-sleep/Leaflet.Sleep.js lib/leaflet.sleep.js
-cp -v node_modules/wmui-base/wmui-base.less lib/wmui-base.less
+cp -v node_modules/leaflet-sleep/Leaflet.Sleep.js lib/external/leaflet.sleep.js
+cp -v node_modules/wmui-base/wmui-base.less lib/external/wmui-base.less
 
 cp -v node_modules/prunecluster/dist/PruneCluster.js \
     node_modules/prunecluster/dist/LeafletStyleSheet.css \
-    lib/prunecluster/
+    lib/external/prunecluster/
 
 node_modules/.bin/rollup --config rollup.config.js
