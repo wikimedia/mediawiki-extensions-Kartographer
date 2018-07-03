d4bf7a191a85082a1e72870c1183117cfc8b7f3d
Michael Holloway <mholloway@wikimedia.org>
2018-07-03 15:52:53 -0400

    Use published version of @wikimedia/mapdata
    Bug: T177019
    Change-Id: I45370665fb6845e0d295c70fb8846bf074004564

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 	var cjs = require( 'rollup-plugin-commonjs' );
 
 	module.exports = {
-		entry: 'node_modules/wikimedia-mapdata/src/index.js',
+		entry: 'node_modules/@wikimedia/mapdata/src/index.js',
 		format: 'cjs',
 		plugins: [ cjs() ],
 		dest: 'lib/external/wikimedia-mapdata.js'
