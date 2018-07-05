fe543daafc586f2cf9cb3a2fad836afaa499873b
Michael Holloway <mholloway@wikimedia.org>
2018-07-05 11:56:09 -0400

    Use published wikimedia fork of Leaflet.Sleep
    Bug: T177019
    Change-Id: I64d0c72fb73c68d6587516c3f23435f35d734193

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ node_modules/.bin/browserify lib/external/mapbox/mapbox-main.js -o lib/external/
 cp -rv node_modules/mapbox.js/theme/* lib/external/mapbox/
 rm lib/external/mapbox/images/render.sh
 
-cp -v node_modules/leaflet-sleep/Leaflet.Sleep.js lib/external/leaflet.sleep.js
+cp -v node_modules/@wikimedia/leaflet-sleep/Leaflet.Sleep.js lib/external/leaflet.sleep.js
 cp -v node_modules/wikimedia-ui-base/wikimedia-ui-base.less lib/external/wikimedia-ui-base.less
 
 cp -v node_modules/prunecluster/dist/PruneCluster.js \
