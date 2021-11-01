c6e87049860e6f75a1fd6e11c1e5f20eafbba8b2
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-11-01 15:33:27 +0100

    Add basic regression tests for projection utility classes
    Change-Id: I6d75f2329c93ed3a5791851fe7f33297ca5561cb

---
+++ tests/phpunit/unit/ProjectionTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Kartographer\UnitTests;
+
+use Kartographer\Projection\EPSG3857;
+use MediaWikiUnitTestCase;
+
+/**
+ * @covers \Kartographer\Projection\EPSG3857
+ * @covers \Kartographer\Projection\SphericalMercator
+ * @covers \Kartographer\Projection\Transformation
+ * @group Kartographer
+ */
+class ProjectionTest extends MediaWikiUnitTestCase {
+
+	private const EARTH_RADIUS = 6378137;
+	private const DELTA = 0.00001;
+
+	public function provideCoordinates() {
+		return [
+			[ [ 0, 0 ], [ 0, 0 ] ],
+			[ [ 1, 1 ], [ 0.017453292519, 0.017454178683 ] ],
+			[ [ 40, 0 ], [ 0, 0.762909652066 ] ],
+			[ [ 80, 180 ], [ pi(), 2.436246053715 ] ],
+			[ [ 0, 360 ], [ 2 * pi(), 0 ] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideCoordinates
+	 */
+	public function testProject( $latLon, $expected ) {
+		$actual = EPSG3857::project( $latLon );
+		$this->assertCount( 2, $actual );
+		$this->assertEqualsWithDelta( $expected[0] * self::EARTH_RADIUS, $actual[0], self::DELTA, 'x' );
+		$this->assertEqualsWithDelta( $expected[1] * self::EARTH_RADIUS, $actual[1], self::DELTA, 'y' );
+	}
+
+	public function provideCoordinatesAndZoom() {
+		return [
+			[ [ 0, 0 ], [ 256, 256 ] ],
+			[ [ 1, 1 ], [ 257.422222221401, 254.577705567489 ] ],
+			[ [ 40, 0 ], [ 256, 193.832528799328 ] ],
+			[ [ 80, 180 ], [ 511.999999852186, 57.476811871679 ] ],
+			[ [ 0, 360 ], [ 767.999999704373, 256 ] ],
+
+			[ [ 0, 0 ], [ 512, 512 ], 2 ],
+			[ [ 1, 1 ], [ 514.844444442802, 509.155411134978 ], 2 ],
+			[ [ 40, 0 ], [ 512, 387.665057598657 ], 2 ],
+			[ [ 80, 180 ], [ 1023.999999704373, 114.953623743359 ], 2 ],
+			[ [ 0, 360 ], [ 1535.999999408747, 512 ], 2 ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideCoordinatesAndZoom
+	 */
+	public function testLatLonProjection( $latLon, $point, $zoom = 1 ) {
+		$actual = EPSG3857::latLonToPoint( $latLon, $zoom );
+		$this->assertCount( 2, $actual );
+		$this->assertEqualsWithDelta( $point[0], $actual[0], self::DELTA, 'x' );
+		$this->assertEqualsWithDelta( $point[1], $actual[1], self::DELTA, 'y' );
+
+		$actual = EPSG3857::pointToLatLon( $point, $zoom );
+		$this->assertCount( 2, $actual );
+		$this->assertEqualsWithDelta( $latLon[0], $actual[0], self::DELTA, 'lat' );
+		$this->assertEqualsWithDelta( $latLon[1], $actual[1], self::DELTA, 'lon' );
+	}
+
+	public function provideZooms() {
+		return [
+			[ -1, 128 ],
+			[ 0, 256 ],
+			[ 0.1, 274.3740064 ],
+			[ 1, 512 ],
+			[ 2, 1024 ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideZooms
+	 */
+	public function testScaleAndSize( $zoom, $expected ) {
+		$actual = EPSG3857::getSize( $zoom );
+		$this->assertCount( 2, $actual );
+		$this->assertSame( $actual[0], $actual[1] );
+		$this->assertEqualsWithDelta( $expected, $actual[0], self::DELTA );
+	}
+
+}
