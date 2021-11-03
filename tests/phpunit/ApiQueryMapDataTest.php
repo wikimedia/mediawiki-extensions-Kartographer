5815815bf410dc16705a26aa47590066316e322b
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-11-03 15:18:26 +0100

    Simplify/cleanup ApiQueryMapDataTest implementation
    Bug: T294734
    Change-Id: Ic0ec32991c4108ad2c9f7225807c3ac0dca8ec18

--- tests/phpunit/ApiQueryMapDataTest.php
+++ tests/phpunit/ApiQueryMapDataTest.php
@@ -5,6 +5,7 @@ namespace Kartographer\Tests;
 use ApiTestCase;
 use CommentStoreComment;
 use MediaWiki\Storage\SlotRecord;
+use WikiPage;
 use WikitextContent;
 
 /**
@@ -15,31 +16,31 @@ use WikitextContent;
  */
 class ApiQueryMapDataTest extends ApiTestCase {
 
-	// phpcs:disable Generic.Files.LineLength
-	private const MAPFRAME_CONTENT = '<mapframe latitude=0 longitude=0 width=1 height=1>{"type": "Feature","geometry": {"type": "Point","coordinates": [1, 2]}}</mapframe>';
-	private const MAPFRAME_JSON = [ '{"_1b3d2dce6411896528c219bf0af1e6c4c3985a1a":[{"type":"Feature","geometry":{"type":"Point","coordinates":[1,2]}}]}' ];
-	private const MAPFRAME_CONTENT_OTHER = '<mapframe latitude=0 longitude=0 width=1 height=1>{"type": "Feature","geometry": {"type": "Point","coordinates": [2, 1]}}</mapframe>';
-	private const MAPFRAME_JSON_OTHER = [ '{"_6602cbfa7dc93b2b4aa360dadf67eb61bf8792a4":[{"type":"Feature","geometry":{"type":"Point","coordinates":[2,1]}}]}' ];
-	// phpcs:enable
+	private const MAPFRAME_JSON = '{"type":"Feature","geometry":{"type":"Point","coordinates":[1,2]}}';
+	private const MAPFRAME_CONTENT = '<mapframe latitude=0 longitude=0 width=1 height=1>' .
+		self::MAPFRAME_JSON . '</mapframe>';
+	private const MAPFRAME_JSON_OTHER = '{"type":"Feature","geometry":{"type":"Point","coordinates":[2,1]}}';
+	private const MAPFRAME_CONTENT_OTHER = '<mapframe latitude=0 longitude=0 width=1 height=1>' .
+		self::MAPFRAME_JSON_OTHER . '</mapframe>';
 
 	public function testExecuteMissingPage() {
-		$apiResult = $this->doApiRequest( [
+		[ $apiResult ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'titles' => __METHOD__,
 		] );
 
-		$this->assertTrue( $apiResult[0]['query']['pages'][-1]['missing'] );
+		$this->assertTrue( $apiResult['query']['pages'][-1]['missing'] );
 	}
 
 	/**
 	 * @dataProvider executeWithTitleProvider
 	 */
-	public function testExecuteWithTitle( $content, $expectedData ) {
+	public function testExecuteWithTitle( string $content, array $expectedData ) {
 		$page = $this->getExistingTestPage( __METHOD__ );
 		$this->addRevision( $page, $content );
 
-		$apiResult = $this->doApiRequest( [
+		[ $apiResult ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'titles' => $page->getDBkey(),
@@ -49,14 +50,30 @@ class ApiQueryMapDataTest extends ApiTestCase {
 	}
 
 	public function executeWithTitleProvider() {
+		$hash = '_' . sha1( '[' . self::MAPFRAME_JSON . ']' );
+
 		return [
-			'No mapframe' => [ 'Nope', [ null ] ],
-			'Empty mapframe' => [ '<mapframe latitude=0 longitude=0 width=1 height=1 />', [ [ '[]' ] ] ],
-			'Map with features' => [ self::MAPFRAME_CONTENT, [ self::MAPFRAME_JSON ] ],
+			'No mapframe' => [
+				'',
+				[]
+			],
+			'Empty mapframe' => [
+				'<mapframe latitude=0 longitude=0 width=1 height=1 />',
+				[ [ '[]' ] ]
+			],
+			'Map with features' => [
+				self::MAPFRAME_CONTENT,
+				[ [ '{"' . $hash . '":[' . self::MAPFRAME_JSON . ']}' ] ]
+			],
 		];
 	}
 
 	public function testExecuteWithMultiple() {
+		$hash = '_' . sha1( '[' . self::MAPFRAME_JSON . ']' );
+		$hashOther = '_' . sha1( '[' . self::MAPFRAME_JSON_OTHER . ']' );
+		$expected = [ '{"' . $hash . '":[' . self::MAPFRAME_JSON . ']}' ];
+		$expectedOther = [ '{"' . $hashOther . '":[' . self::MAPFRAME_JSON_OTHER . ']}' ];
+
 		$pageOne = $this->getExistingTestPage( __METHOD__ );
 		$oldRevPageOne = $this->addRevision( $pageOne, self::MAPFRAME_CONTENT_OTHER );
 		$currRevPageOne = $this->addRevision( $pageOne, self::MAPFRAME_CONTENT );
@@ -65,57 +82,49 @@ class ApiQueryMapDataTest extends ApiTestCase {
 		$currRevPageTwo = $this->addRevision( $pageTwo, self::MAPFRAME_CONTENT_OTHER );
 
 		// query two different pages
-		$apiResultTitles = $this->doApiRequest( [
+		[ $apiResultTitles ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'titles' => $pageOne->getDBkey() . '|' . $pageTwo->getDBkey(),
 		] );
-		$this->assertResult( [ self::MAPFRAME_JSON, self::MAPFRAME_JSON_OTHER ], $apiResultTitles );
+		$this->assertResult( [ $expected, $expectedOther ], $apiResultTitles );
 
 		// query a single revision
-		$apiResultOneRevision = $this->doApiRequest( [
+		[ $apiResultOneRevision ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'revids' => $currRevPageOne->getId(),
 		] );
-		$this->assertResult( [ self::MAPFRAME_JSON ], $apiResultOneRevision );
+		$this->assertResult( [ $expected ], $apiResultOneRevision );
 
 		// query two different revisions from two differrent pages
-		$apiResultRevisions = $this->doApiRequest( [
+		[ $apiResultRevisions ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'revids' => $currRevPageOne->getId() . '|' . $currRevPageTwo->getId(),
 		] );
-		$this->assertResult( [ self::MAPFRAME_JSON, self::MAPFRAME_JSON_OTHER ], $apiResultRevisions );
+		$this->assertResult( [ $expected, $expectedOther ], $apiResultRevisions );
 
 		// using an old revision from the same page still just returns the data from the latest revision
-		$apiResultOldRevision = $this->doApiRequest( [
+		[ $apiResultOldRevision ] = $this->doApiRequest( [
 			'action' => 'query',
 			'prop' => 'mapdata',
 			'revids' => $oldRevPageOne->getId(),
 		] );
-		// with old revids working this should return `[ self::MAPFRAME_JSON_OTHER ]`
-		$this->assertResult( [ self::MAPFRAME_JSON ], $apiResultOldRevision );
+		// with old revids working this should return `[ $expectedOther ]`
+		$this->assertResult( [ $expected ], $apiResultOldRevision );
 	}
 
-	private function addRevision( $page, $content ) {
+	private function addRevision( WikiPage $page, string $wikitext ) {
 		return $page->newPageUpdater( $this->getTestUser()->getUser() )
-			->setContent( SlotRecord::MAIN, new WikitextContent( $content ) )
+			->setContent( SlotRecord::MAIN, new WikitextContent( $wikitext ) )
 			->saveRevision( CommentStoreComment::newUnsavedComment( __CLASS__ ) );
 	}
 
-	private function assertResult( array $expectedMapdata, $apiResult ) {
-		$i = 0;
-		foreach ( $apiResult[0]['query']['pages'] as $page ) {
-			if ( $expectedMapdata[$i] !== null ) {
-				$this->assertArrayHasKey( 'mapdata', $page );
-				$this->assertArrayEquals( $expectedMapdata[$i], $page['mapdata'] );
-			} else {
-				$this->assertArrayNotHasKey( 'mapdata', $page );
-			}
-			$i++;
-		}
-
-		$this->assertSame( count( $expectedMapdata ), $i, 'number of results as expected' );
+	private function assertResult( array $expectedMapData, array $apiResult ) {
+		$this->assertSame(
+			$expectedMapData,
+			array_column( $apiResult['query']['pages'], 'mapdata' )
+		);
 	}
 }
