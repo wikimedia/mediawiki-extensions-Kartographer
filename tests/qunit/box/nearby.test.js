48eb76302fc01a37182ee3a04f9fc92846e2384f
Andrew Kostka <andrew.kostka@wikimedia.de>
2022-04-21 14:40:55 +0200

    Add tests for the nearby geosearch query
    Bug: T304075
    Change-Id: I0fd519fe7e165097621392809a8b53f3c70d6b50

--- tests/qunit/box/nearby.test.js
+++ tests/qunit/box/nearby.test.js
@@ -1,59 +1,85 @@
 ( function () {
-	QUnit.module( 'ext.kartographer.box.nearby', QUnit.newMwEnvironment() );
+	// Truncated response adapted from https://en.m.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=coordinates|pageprops|pageprops|pageimages|description&colimit=max&generator=geosearch&ggsradius=10000&ggsnamespace=0&ggslimit=50&ggscoord=43.7383|7.4137&ppprop=displaytitle&piprop=thumbnail&pithumbsize=150&pilimit=50 . Replace with live data.
+	const dummyGeosearchResponse = {
+		query: {
+			pages: [
+				{
+					pageid: 19261,
+					ns: 0,
+					title: 'Monaco',
+					index: -1,
+					coordinates: [
+						{
+							lat: 43.2,
+							lon: 7.1,
+							primary: true,
+							globe: 'earth'
+						}
+					],
+					thumbnail: {
+						source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Flag_of_Monaco.svg/150px-Flag_of_Monaco.svg.png',
+						width: 150,
+						height: 120
+					},
+					description: 'City-state and microstate on the French Riviera',
+					descriptionsource: 'local'
+				},
+				{
+					pageid: 658766,
+					ns: 0,
+					title: 'La Condamine',
+					index: 0,
+					coordinates: [
+						{
+							lat: 43.7,
+							lon: 7.4,
+							primary: true,
+							globe: 'earth'
+						}
+					],
+					thumbnail: {
+						source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Monaco_-_Port_en_d%C3%A9cembre.jpg/150px-Monaco_-_Port_en_d%C3%A9cembre.jpg',
+						width: 150,
+						height: 100
+					},
+					description: 'Ward of Monaco',
+					descriptionsource: 'local'
+				}
+			]
+		}
+	};
+
+	QUnit.module( 'ext.kartographer.box.nearby', QUnit.newMwEnvironment( {
+		beforeEach() {
+			this.server = this.sandbox.useFakeServer();
+			this.server.respondImmediately = true;
+		}
+	} ) );
+
+	QUnit.test( 'Executes a valid geosearch request', function ( assert ) {
+		this.server.respond(
+			[ 200, { 'Content-Type': 'application/json' }, JSON.stringify( dummyGeosearchResponse ) ]
+		);
+
+		const Nearby = require( 'ext.kartographer.box' ).private.Nearby;
+		const bounds = L.latLngBounds( L.latLng( 40.712, -74.227 ), L.latLng( 40.774, -74.125 ) );
+		const done = assert.async();
+
+		const expectedApiUrl = mw.config.get( 'wgScriptPath' ) + '/api.php?action=query&format=json&formatversion=2&prop=coordinates%7Cpageprops%7Cpageimages%7Cdescription&colimit=max&generator=geosearch&ggsbbox=40.774%7C-74.227%7C40.712%7C-74.125&ggsnamespace=0&ggslimit=50&ppprop=displaytitle&piprop=thumbnail&pithumbsize=150&pilimit=50';
+
+		Nearby.fetch( bounds ).then( ( nearbyResults ) => {
+			assert.deepEqual( nearbyResults, dummyGeosearchResponse );
+
+			const requests = this.server.requests;
+			assert.strictEqual( requests.length, 1 );
+			assert.strictEqual( requests[ 0 ].url, expectedApiUrl );
+			done();
+		} );
+	} );
 
 	QUnit.test( 'Converts valid geosearch response', function ( assert ) {
-		// Truncated response adapted from https://en.m.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=coordinates|pageprops|pageprops|pageimages|description&colimit=max&generator=geosearch&ggsradius=10000&ggsnamespace=0&ggslimit=50&ggscoord=43.7383|7.4137&ppprop=displaytitle&piprop=thumbnail&pithumbsize=150&pilimit=50 . Replace with live data.
-		var dummyGeosearchResponse = {
-			query: {
-				pages: [
-					{
-						pageid: 19261,
-						ns: 0,
-						title: 'Monaco',
-						index: -1,
-						coordinates: [
-							{
-								lat: 43.2,
-								lon: 7.1,
-								primary: true,
-								globe: 'earth'
-							}
-						],
-						thumbnail: {
-							source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Flag_of_Monaco.svg/150px-Flag_of_Monaco.svg.png',
-							width: 150,
-							height: 120
-						},
-						description: 'City-state and microstate on the French Riviera',
-						descriptionsource: 'local'
-					},
-					{
-						pageid: 658766,
-						ns: 0,
-						title: 'La Condamine',
-						index: 0,
-						coordinates: [
-							{
-								lat: 43.7,
-								lon: 7.4,
-								primary: true,
-								globe: 'earth'
-							}
-						],
-						thumbnail: {
-							source: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Monaco_-_Port_en_d%C3%A9cembre.jpg/150px-Monaco_-_Port_en_d%C3%A9cembre.jpg',
-							width: 150,
-							height: 100
-						},
-						description: 'Ward of Monaco',
-						descriptionsource: 'local'
-					}
-				]
-			}
-		};
-
-		var Nearby = require( 'ext.kartographer.box' ).private.Nearby;
-		var expectedGeojson = [
+		const Nearby = require( 'ext.kartographer.box' ).private.Nearby;
+		const expectedGeojson = [
 			{
 				geometry: {
 					coordinates: [
