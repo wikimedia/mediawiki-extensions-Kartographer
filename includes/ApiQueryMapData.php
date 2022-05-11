53815c19f8a2cc043453e6cfd1466a1e3609ec94
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2022-05-11 13:59:05 +0200

    Duplicate "latest revision may be special" logic from FlaggedRevs
    Patchset 2 is optimized for performance.
    
    Bug: T304813
    Change-Id: Ida08ae2b37139737dfde3816b3c8afbaa4541a2a

--- includes/ApiQueryMapData.php
+++ includes/ApiQueryMapData.php
@@ -181,8 +181,12 @@ class ApiQueryMapData extends ApiQueryBase {
 						return $parserOutput;
 					}
 
-					// Cache misses should be rare, since the main page has been recently viewed.
-					LoggerFactory::getInstance( 'Kartographer' )->notice( 'Stable cache miss' );
+					// A cache miss is expected in case everything is in sync. We just want to do
+					// this expensive check as late as possible.
+					if ( !$page->stableVersionIsSynced() ) {
+						// Cache misses should be rare, since the main page has been recently viewed.
+						LoggerFactory::getInstance( 'Kartographer' )->notice( 'Stable cache miss' );
+					}
 				}
 			}
 		} else {
