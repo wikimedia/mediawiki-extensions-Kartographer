5d477de1e90bf73b754b30bfc7e89923dddb21af
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2022-09-19 22:45:18 +0200

    Add unit test for ExternalLinksProvider
    While writing the test I discovered a minor issue. The "types" array
    was returned as a sparse array (with some numeric keys missing)
    because of array_unique(). Fixed by changing the execution order a
    little.
    
    Also rename a variable to $usedTypes to reflect better what this is.
    
    Change-Id: I7eb11c5bef6ec64abbe209880fde6f57d7a62262

---
+++ tests/phpunit/unit/ExternalLinksProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kartographer\UnitTests;
+
+use Kartographer\ExternalLinksProvider;
+use MediaWiki\ResourceLoader\Context;
+
+/**
+ * @covers \Kartographer\ExternalLinksProvider
+ * @group Kartographer
+ */
+class ExternalLinksProviderTest extends \MediaWikiUnitTestCase {
+
+	public function testGetData() {
+		$context = $this->createMock( Context::class );
+		$context->method( 'msg' )->willReturnCallback( function ( $key ) {
+			$msg = $this->createMock( \Message::class );
+			$msg->method( 'plain' )->willReturn( "<$key>" );
+			return $msg;
+		} );
+
+		$data = ExternalLinksProvider::getData( $context );
+
+		$this->assertInstanceOf( \stdClass::class, $data );
+		$this->assertIsArray( $data->types );
+		// TODO: Can be replaced with array_is_list() when we can use PHP 8.1
+		$this->assertSame( array_values( $data->types ), $data->types );
+		$this->assertIsArray( $data->localization );
+		$this->assertLessThanOrEqual( count( $data->types ), count( $data->localization ) );
+
+		// Check for an example value
+		$this->assertContains( 'map', $data->types );
+		$this->assertSame( '<kartographer-linktype-map>', $data->localization['map'] );
+	}
+
+}
