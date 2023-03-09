f6f1f7afa75bd85524ec3fd53c289cbab796629d
thiemowmde <thiemo.kreuz@wikimedia.de>
2023-03-09 13:15:10 +0100

    Extract separate little ErrorReporter service
    The main benefit is that this removes a good chunk of really
    isolated code from the abstract base class. This makes it easier
    to test, and more obvious what the (remaining) purpose of the base
    class is. Long-term goal is to possibly get rid of the base class.
    
    Re-use is not a concern as this code can't be reused for the
    Parsoid port anyway.
    
    Change-Id: I58ec0ee28b017007ac74098b1c0225f6effecd34

---
+++ includes/Tag/ErrorReporter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Kartographer\Tag;
+
+use Html;
+use InvalidArgumentException;
+use StatusValue;
+
+/**
+ * @license MIT
+ */
+class ErrorReporter {
+
+	private string $language;
+
+	public function __construct( string $languageCode ) {
+		$this->language = $languageCode;
+	}
+
+	/**
+	 * @param StatusValue $status
+	 * @param string $tag
+	 * @return string HTML
+	 */
+	public function getHtml( StatusValue $status, string $tag ): string {
+		$errors = array_merge(
+			$status->getErrorsByType( 'error' ),
+			$status->getErrorsByType( 'warning' )
+		);
+		if ( !$errors ) {
+			throw new InvalidArgumentException( 'Attempt to report error when none took place' );
+		}
+
+		if ( count( $errors ) > 1 ) {
+			$html = '';
+			foreach ( $errors as $err ) {
+				$html .= Html::rawElement( 'li', [], wfMessage( $err['message'], $err['params'] )
+					->inLanguage( $this->language )->parse() ) . "\n";
+			}
+			$msg = wfMessage( 'kartographer-error-context-multi', "<$tag>" )
+				->rawParams( Html::rawElement( 'ul', [], $html ) );
+		} else {
+			$errorText = wfMessage( $errors[0]['message'], $errors[0]['params'] )
+				->inLanguage( $this->language )->parse();
+			$msg = wfMessage( 'kartographer-error-context', "<$tag>" )
+				->rawParams( $errorText );
+		}
+
+		return Html::rawElement( 'div', [ 'class' => 'mw-kartographer-error' ],
+			$msg->inLanguage( $this->language )->escaped() .
+			$this->getJSONValidatorLog( $status->getValue()['schema-errors'] ?? [] )
+		);
+	}
+
+	/**
+	 * @param array[] $errors
+	 * @return string HTML
+	 */
+	private function getJSONValidatorLog( array $errors ): string {
+		if ( !$errors ) {
+			return '';
+		}
+
+		$log = "\n";
+		/** These errors come from {@see \JsonSchema\Constraints\BaseConstraint::addError} */
+		foreach ( $errors as $error ) {
+			$log .= Html::element( 'li', [],
+				$error['pointer'] . wfMessage( 'colon-separator' )->text() . $error['message']
+			) . "\n";
+		}
+		return Html::rawElement( 'ul', [ 'class' => [
+			'mw-kartographer-error-log',
+			'mw-collapsible',
+			'mw-collapsed',
+		] ], $log );
+	}
+
+}
