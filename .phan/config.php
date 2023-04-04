cac746220b68a14b5ae3da687991a07cd29873aa
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2023-04-04 10:17:43 +0200

    Add Parsoid-specific aliases to Phan configuration
    We need to alias DOM classes between Parsoid and the DOM API to make
    Phan happy. This is what's done, among others, in DiscussionTools and in
    the Parsoid repo itself.
    
    Bug: T263762
    Change-Id: Ida6d7fd193743e277353860c4bbc227990c31543

--- .phan/config.php
+++ .phan/config.php
@@ -20,4 +20,28 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	]
 );
 
+/**
+ * Quick implementation of a recursive directory list.
+ * @param string $dir The directory to list
+ * @param ?array &$result Where to put the result
+ */
+function wfCollectPhpFiles( string $dir, ?array &$result = [] ) {
+		if ( !is_dir( $dir ) ) {
+				return;
+		}
+		foreach ( scandir( $dir ) as $f ) {
+				if ( $f === '.' || $f === '..' ) {
+						continue;
+				}
+				$fullName = $dir . DIRECTORY_SEPARATOR . $f;
+				wfCollectPhpFiles( $fullName, $result );
+				if ( is_file( $fullName ) && preg_match( '/\.php$/D', $fullName ) ) {
+						$result[] = $fullName;
+				}
+		}
+}
+
+// Exclude src/DOM in favour of .phan/stubs/DomImpl.php
+wfCollectPhpFiles( "{$VP}/vendor/wikimedia/parsoid/src/DOM", $cfg['exclude_file_list'] );
+
 return $cfg;
