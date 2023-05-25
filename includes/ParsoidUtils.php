fc96e2753d5ee45c183231e19dc344c633aa6782
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2023-05-25 10:56:27 +0200

    Use page language rather than user language
    We expect to use these messages in the context of a wiki page, where the
    page language yields more consistent language use on wiki than the user
    language. The full proposal for languages in Kartographer is available
    here:
    https://phabricator.wikimedia.org/T309166#8856291
    
    We'll display page-internal messages in the page language rather than in
    the user language (unless specified otherwise); this patch adjusts the
    language fragment util for this, and creates a lang attribute method
    that can be used to internationalize attributes (such as alt
    attributes).
    
    Bug: T263762
    Bug: T309166
    Change-Id: I50e54e83e5f032817ed510ebc2b86e5edd85f7a4

--- includes/ParsoidUtils.php
+++ includes/ParsoidUtils.php
@@ -4,6 +4,7 @@ namespace Kartographer;
 
 use Wikimedia\Bcp47Code\Bcp47CodeValue;
 use Wikimedia\Parsoid\DOM\DocumentFragment;
+use Wikimedia\Parsoid\DOM\Element;
 use Wikimedia\Parsoid\Ext\ParsoidExtensionAPI;
 
 /**
@@ -13,7 +14,7 @@ class ParsoidUtils {
 
 	/**
 	 * Creates an internationalized Parsoid fragment. If $language is not provided, the returned
-	 * fragment is generated in the user interface language.
+	 * fragment is generated in the page language.
 	 * @param string $msgKey
 	 * @param array $params
 	 * @param ParsoidExtensionAPI $extApi
@@ -24,8 +25,29 @@ class ParsoidUtils {
 		string $msgKey, array $params, ParsoidExtensionAPI $extApi, ?string $language
 	): DocumentFragment {
 		if ( $language === null ) {
-			return $extApi->createInterfaceI18nFragment( $msgKey, $params );
+			return $extApi->createPageContentI18nFragment( $msgKey, $params );
 		}
 		return $extApi->createLangI18nFragment( new Bcp47CodeValue( $language ), $msgKey, $params );
+	}
+
+	/**
+	 * Creates an internationalized Parsoid attribute on the provided element. If $language is
+	 * not provided, the created attribute is generated in the page language.
+	 * @param Element $element
+	 * @param string $name
+	 * @param string $msgKey
+	 * @param array $params
+	 * @param ParsoidExtensionAPI $extApi
+	 * @param string|null $language
+	 * @return void
+	 */
+	public static function createLangAttribute(
+		Element $element, string $name, string $msgKey, array $params, ParsoidExtensionAPI $extApi, ?string $language
+	): void {
+		if ( $language === null ) {
+			$extApi->addPageContentI18nAttribute( $element, $name, $msgKey, $params );
+		} else {
+			$extApi->addLangI18nAttribute( $element, new Bcp47CodeValue( $language ), $name, $msgKey, $params );
+		}
 	}
 }
